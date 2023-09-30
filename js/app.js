9eb309524f5d902afde3730dd303e0a1f6ad5e8f
Luca Viana <78035167+O-orion@users.noreply.github.com>
2023-09-30 18:47:34 -0300

    melhorias

--- js/app.js
+++ js/app.js
@@ -1,31 +1,44 @@
-const produtoCampo = document.querySelector('#produto')
-const quantidade = document.querySelector('#quantidade')
-const listaCarrinho = document.querySelector('#lista-produtos')
-const total = document.querySelector('#valor-total')
+const produtoCampo = document.querySelector('#produto');
+const quantidade = document.querySelector('#quantidade');
+const listaCarrinho = document.querySelector('#lista-produtos');
+const total = document.querySelector('#valor-total');
 
 function adicionar() {
-    const produto = produtoCampo.value
-    const textoProduto = produto.split('-')
-    const valor = textoProduto[1].substring(3)
-    const quantidadeProduto = quantidade.value
+    const produto = produtoCampo.value;
+    const textoProduto = produto.split('-');
+    const valor = parseFloat(textoProduto[1].substring(3));
+    const quantidadeProduto = parseInt(quantidade.value);
 
-    listaCarrinho.innerHTML += `
-    <section class='carrinho__produtos__produto'>
-        <span class='texto-azul'>${quantidadeProduto}x</span> 
-        ${textoProduto[0]}
-        <span class='texto-azul'>${textoProduto[1]}</span> 
+    if (quantidadeProduto <= 0 || isNaN(quantidadeProduto)) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
 
-    
-    </section>
-    `
+    const produtoElement = document.createElement('section');
+    produtoElement.className = 'carrinho__produtos__produto';
 
-    const valorTotal =  Number(total.textContent.substring(2)) + (Number(valor) * quantidadeProduto);
-    total.textContent = `R$${valorTotal}`
-    console.log(total.textContent.substring(2))
+    const quantidadeSpan = document.createElement('span');
+    quantidadeSpan.className = 'texto-azul';
+    quantidadeSpan.textContent = `${quantidadeProduto}x`;
+
+    const nomeProduto = document.createTextNode(textoProduto[0]);
+
+    const valorSpan = document.createElement('span');
+    valorSpan.className = 'texto-azul';
+    valorSpan.textContent = textoProduto[1];
+
+    produtoElement.appendChild(quantidadeSpan);
+    produtoElement.appendChild(nomeProduto);
+    produtoElement.appendChild(valorSpan);
+
+    listaCarrinho.appendChild(produtoElement);
+
+    const valorTotal = parseFloat(total.textContent.substring(2)) + (valor * quantidadeProduto);
+    total.textContent = `R$${valorTotal.toFixed(2)}`;
 }
 
 function limpar() {
-    quantidade.value = ''
-    listaCarrinho.innerHTML = ''
-    total.textContent = ''
+    quantidade.value = '';
+    listaCarrinho.innerHTML = '';
+    total.textContent = 'R$0.00';
 }
